558352cf9beb0e08fd64b741970fa5743d797580
Adorilson Bezerra <adorilson@gmail.com>
2011-12-20 07:49:07 -0300

    conclusao da apresentacao

---
+++ trabalhofinal/MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass
+{
+   public static final boolean  HAS_CHEEZBURGER=true;
+
+   public void someMethod()
+   {
+       if( MyClass.HAS_CHEEZBURGER)
+       {
+           doCheezburger();
+       } else
+       {
+           noCheezBurger();
+       }
+   }
+
+}
+
